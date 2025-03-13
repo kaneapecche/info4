8bba754c25850fa6b55a9dfa05edda29bf82ac8b
kaneapecche <codou.kne@gmail.com>
2025-03-13 08:31:17 +0100

    Update and rename inscription.html to inscription.php

--- projet.html/inscription.php
+++ projet.html/inscription.php
@@ -4,9 +4,8 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>SereniTrip</title>
-   <link rel="shortcut icon" href="image/logo.png" type="image/x-icon">
+   <link rel="shortcut icon" href="logo.png" type="image/x-icon">
    <link rel="stylesheet" href="projet.css/root.css">
-   <link rel="stylesheet" href="projet.css/login.css">
    <link rel="stylesheet" href="projet.css/apart.css">
 </head>
 <body>
@@ -14,46 +13,38 @@
         <img src="image/logo.png" alt="logo du site web" width="100" class="image">
         <div class="menu">
         <ul>
-            <li><a href="accueil.html">Accueil</a></li>
-            <li><a href="présentation.html">Destination</a></li>
-            <li><a href="connexion.html">Connexion</a></li>
-            <li><a href="profil.html">Profil</a></li>
+            <li><a href="accueil.php">Accueil</a></li>
+            <li><a href="présentation.php">Destination</a></li>
+            <li><a href="connexion.php">Connexion</a></li>
         </ul>
         </div>
     </div>
     <br>
-   <div class="container">
-      <fieldset class="center-form">
+   <div class="container"><fieldset class="center-form">
        <legend>Inscription</legend>
-       <form action="https://www.cafe-it.fr/cytech/post.php" method="post" >
+       <form action="verif_inscription.php" method="post" >
            <label for="nom">Nom:</label>
            <input class="fill" type="text" name="nom">
            <label for="prenom">Prenom:</label>
-           <input class="fill" type="text" name="nom">
+           <input class="fill" type="text" name="prenom">
            <br>
            <label for="email">Adresse e-mail</label>
            <input class="fill" type="email" name="email">
-           <br><br>
+           <br>
            <label for="birthday">Date de naissance</label>
            <input type="date" name="birthday" value="2023-06-03">
-           <br><br>
-           <label for="sexe">Genre</label>
-           <input type="radio" name="sexe" value="femme" checked="checked">Femme
-           <input type="radio" name="sexe" value="homme">Homme
-           <br>
+           <label for="genre">Genre</label>
+           <input type="radio" name="genre" value="femme" checked="checked">Femme
+           <input type="radio" name="genre" value="homme">Homme
            <br>
            <label for="phone">Téléphone</label>
            <input class="fill" type="tel" name="phone">
-           <br><br>
+           <br>
            <label for="password">Mot de passe</label>
            <input class="fill" type="password" name="password">
-           <br><br>
-           <input class="button" type="submit" value="s'inscrire" formaction="profil.html">
+           <br>
+           <input class="button" type="submit" value="s'inscrire">
        </form>
-       <ul class="center-list">
-         <li><a href="connexion.html">incription</a></li>
-       </ul>
-   </fieldset>
-</div>
+   </fieldset></div>
 </body>
-</html>v
+</html>
