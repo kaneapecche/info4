44e594b8104aec6cb6cc4fdc3d3a5df58ddc6299
kaneapecche <codou.kne@gmail.com>
2025-03-25 14:26:22 +0100

    Update accueil.php

--- htdocs/projet.html/accueil.php
+++ htdocs/projet.html/accueil.php
@@ -1,4 +1,4 @@
-
+<?php session_start() ?>
    <?php
 $json = file_get_contents('donnees/voyages.json');
 $voyages = json_decode($json, true);
@@ -27,11 +27,18 @@ $voyagesAlaUne = array_slice($voyages, 0, 5);
    <img src="image/logo.png" alt="logo du site web" width="100" class="image">
    <div class="menu">
 <ul>
-   <li><a href="accueil.php">Accueil</a></li>
-   <li><a href="présentation.php">Destination</a></li>
-   <li><a href="connexion.php">Connexion</a></li>
-   <li><a href="profil.php">Profil</a></li>
-</ul>
+            <li><a href="accueil.php" class="button">Accueil</a></li>
+            <li><a href="présentation.php">Destination</a></li>
+
+            <?php if(!isset($_SESSION["login"])): ?>
+                <li><a href="connexion.php">Connexion</a></li>
+            <?php endif; ?>
+
+            <?php if(isset($_SESSION["login"])): ?>
+                <li><a href="profil.php">Profil</a></li>
+                <li><a href="logout.php">Déconnexion</a></li>
+            <?php endif; ?>
+        </ul>
 
 
 <h1><i>Bienvenue à SereniTrip</i></h1>
