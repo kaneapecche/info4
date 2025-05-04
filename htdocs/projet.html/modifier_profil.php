4f3fd0db69c6955672f47f2c26553dd863a41cc4
kaneapecche <codou.kne@gmail.com>
2025-05-04 16:29:42 +0200

    Update modifier_profil.php

--- htdocs/projet.html/modifier_profil.php
+++ htdocs/projet.html/modifier_profil.php
@@ -1,62 +1,41 @@
+
 <?php
 session_start();
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<?php 
-
-var_dump($_SESSION);
-exit();
 if (!isset($_SESSION['login'])) {
-    header("Location: connexion.php");
-    exit();
+    header('Location: connexion.php');
+    exit;
 }
 
-$user_login = $_SESSION['login'];
-$fichier = 'donnees/utilisateurs.csv';
-$temp_fichier = 'donnees/temp_utilisateurs.csv';
+$login = $_SESSION['login'];
+$new_nom = $_POST['nom'];
+$new_prenom = $_POST['prenom'];
+$new_email = $_POST['email'];
+$new_phone = $_POST['phone'];
+$new_password = $_POST['password'];
 
-// Vérifier si le formulaire a été soumis
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nouveau_nom = $_POST['nom'];
-    $nouveau_prenom = $_POST['prenom'];
-    $nouveau_email = $_POST['email'];
-    $nouveau_telephone = $_POST['phone'];
-    $nouvelle_date = $_POST['birthday'];
-    $nouveau_genre = $_POST['genre'];
-    $nouveau_mdp = $_POST['password'];
+// Lecture de tous les utilisateurs
+$users = array_map('str_getcsv', file('donnees/utilisateurs.csv'));
 
-    $updated = false;
-
-    if (($handle = fopen($fichier, "r")) !== FALSE && ($temp = fopen($temp_fichier, "w")) !== FALSE) {
-        while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-            if ($data[6] == $user_login) { 
-                $data = [$nouveau_nom, $nouveau_prenom, $nouveau_email, $nouveau_telephone, $nouvelle_date, $nouveau_genre, $user_login, $nouveau_mdp];
-                $updated = true;
-            }
-            fputcsv($temp, $data, ";");
-        }
-        fclose($handle);
-        fclose($temp);
-
-        if ($updated) {
-            rename($temp_fichier, $fichier); // Remplace l'ancien fichier
-            header("Location: profil.php"); // Redirige vers le profil après mise à jour
-            exit();
-        } else {
-            unlink($temp_fichier); // Supprime le fichier temporaire si aucun changement
-        }
+// Mise à jour
+foreach ($users as &$user) {
+    if ($user[6] === $login) { // 3 = pseudo
+        $user[0] = $new_nom;
+        $user[1] = $new_prenom;
+        $user[2] = $new_email;
+        $user[3] = $new_phone;
+        $user[7] = $new_password; // ATTENTION : ici tu pourrais hasher le mot de passe pour plus de sécurité
+        $user[10] = date('Y-m-d H:i:s'); // Mise à jour dernière modification
+        break;
     }
 }
-header("Location: profil.php?edit=true");
-exit();
+
+// Réécriture du fichier CSV
+$file = fopen('donnees/utilisateurs.csv', 'w');
+foreach ($users as $user) {
+    fputcsv($file, $user);
+}
+fclose($file);
+
+header('Location: profil.php');
+exit;
 ?>
-</body>
-</html>
