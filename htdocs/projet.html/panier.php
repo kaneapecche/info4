a33e3c42f1d859f4ee58f5ae57cbad3e067af75d
mejdo5 <mejdoline_ketata@yahoo.com>
2025-05-17 21:51:36 +0200

    Update panier.php

--- htdocs/projet.html/panier.php
+++ htdocs/projet.html/panier.php
@@ -31,6 +31,24 @@ if (empty($panier)) {
   <option value="projet.css/style-dark.css">Sombre</option>
   <option value="projet.css/style-accessible.css">Malvoyant</option>
 </select>
+<div class="navigation">
+        <img src="image/logo.png" alt="logo du site web" width="100" class="image">
+        <div class="menu">
+<ul>
+            <li><a href="accueil.php">Accueil</a></li>
+            <li><a href="présentation.php">Destination</a></li>
+
+            <?php if(!isset($_SESSION["login"])): ?>
+                <li><a href="connexion.php">Connexion</a></li>
+                <?php endif; ?>
+
+                <?php if(isset($_SESSION["login"])): ?>
+                <li><a href="profil.php">Profil</a></li>
+                <li><a href="logout.php">Déconnexion</a></li>
+                <?php endif; ?>
+        </ul>
+        </div>
+    </div>
 <h1>🧳 Mon Panier</h1>
 
 <?php
@@ -57,30 +75,37 @@ foreach ($panier as $id) {
 
         echo "<h4>🗺️ Détails personnalisés :</h4><ul>";
 
+        echo '<div class="accueil">';
         foreach ($voyage['etapes'] as $index => $etape) {
             if (isset($perso['etapes_selectionnees'][$index])) {
                 $nb = $perso['nb_personnes'][$index] ?? 1;
                 $prix = $etape['prix'] * $nb;
                 $total_personnalise += $prix;
-
-                echo "<li><strong>" . htmlspecialchars($etape['titre']) . "</strong><br>";
+        
+                echo '<div class="accueil-card">';
+                echo "<strong>" . htmlspecialchars($etape['titre']) . "</strong><br>";
                 echo "👥 Personnes : $nb<br>";
                 echo "🏨 Hébergement : " . htmlspecialchars($etape['hebergement']) . "<br>";
                 echo "🎭 Activités : " . htmlspecialchars($etape['activites']) . "<br>";
                 echo "🚍 Transport : " . htmlspecialchars($etape['transport']) . "<br>";
-                echo "💰 Prix : $prix €</li><br>";
+                echo "💰 Prix : $prix €";
+                echo '</div>';
             }
         }
-
-        echo "</ul>";
+        echo '</div>';
+        
     }
 
+        echo "</ul>";
+}
+    
+    
     echo "<strong>💶 Prix total personnalisé : $total_personnalise €</strong><br>";
     echo "<a href='supprimer_panier.php?id=$id'>❌ Supprimer</a>";
     echo "</div><hr>";
 
     $prix_total_general += $total_personnalise;
-}
+
 
 echo "<h3>💸 Total général : $prix_total_general €</h3>";
 echo "<a href='paiement.php?id=$id'>Valider et payer</a>";
